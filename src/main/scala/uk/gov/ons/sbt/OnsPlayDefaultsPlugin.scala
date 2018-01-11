81ee4e82a6d7f60e0c1c9f7470ce710856a4e1cb
Chris Cundill <chrisc@diligentsoft.co>
2018-01-11 14:39:06 +0000

    REG-42 Remedial action following testing with sbr-admin-data

--- src/main/scala/uk/gov/ons/sbt/OnsPlayDefaultsPlugin.scala
+++ src/main/scala/uk/gov/ons/sbt/OnsPlayDefaultsPlugin.scala
@@ -2,23 +2,20 @@ package uk.gov.ons.sbt
 
 import sbt.Keys._
 import sbt._
-import sbtassembly.AssemblyPlugin.autoImport.{MergeStrategy, assembly, assemblyJarName, assemblyMergeStrategy}
-import sbtassembly.{AssemblyPlugin, PathList}
 import play.routes.compiler.InjectedRoutesGenerator
-import play.sbt.PlayImport.{PlayKeys, cache, filters, ws}
+import play.sbt.PlayImport.{cache, filters, ws}
 import play.sbt.PlayScala
 import play.sbt.routes.RoutesKeys.{routesGenerator, routesImport}
-import com.typesafe.sbt.packager.universal.UniversalPlugin
 
 object OnsPlayDefaultsPlugin extends AutoPlugin {
 
   override def trigger = allRequirements
-  override def requires = PlayScala && AssemblyPlugin && UniversalPlugin
+  override def requires = PlayScala
 
   override lazy val projectSettings = {
     libraryDependencySettings ++
     playSettings ++
-    assemblySettings
+    testSettings
   }
 
   override lazy val projectConfigurations = Seq(ITest)
@@ -57,26 +54,12 @@ object OnsPlayDefaultsPlugin extends AutoPlugin {
     "org.scalatestplus.play"     %%  "scalatestplus-play" % "2.0.0" % "test"
   )
 
-  lazy val testSettings = Seq(
+  private[this] lazy val testSettings = Seq(
     sourceDirectory in ITest := baseDirectory.value / "test/controllers/v1/it",
     resourceDirectory in ITest := baseDirectory.value / "test/resources",
     scalaSource in ITest := baseDirectory.value / "test/controllers/v1/it",
     // test setup
     parallelExecution in Test := false
-  )
-
-  lazy val assemblySettings = Seq(
-    assemblyJarName in assembly := s"${name.value}-assembly-${version.value}.jar",
-    assemblyMergeStrategy in assembly := {
-      case PathList("org", "apache", xs@_*)                             => MergeStrategy.last
-      case PathList("META-INF", "io.netty.versions.properties", xs@_ *) => MergeStrategy.last
-      case "application.conf"                                           => MergeStrategy.first
-      case x =>
-        val oldStrategy = (assemblyMergeStrategy in assembly).value
-        oldStrategy(x)
-    },
-    mainClass in assembly := Some("play.core.server.ProdServerStart"),
-    fullClasspath in assembly += Attributed.blank(PlayKeys.playPackageAssets.value)
   )
 
 }
